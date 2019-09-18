8ca8b66db3707c5892269350d5e8930569bdb267
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2019-09-18 15:08:34 +0200

    Upgrade scala check

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
-  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
+  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.1"
   lazy val sortImports = "com.nequissimus" %% "sort-imports" % "0.2.1"
 }
